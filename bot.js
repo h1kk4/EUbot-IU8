422e24b6009ab53098aea79480322685c53a2b12
pettro98 <ya.skywalker@yandex.ru>
2018-01-12 02:13:32 +0300

    upd

--- bot.js
+++ bot.js
@@ -1,26 +1,75 @@
 "use strict";
 
-const API = require("node-vk-bot-api");
-const bot = new API(process.env.TOKEN);
+// const API = require("node-vk-bot-api");
+// const bot = new API(process.env.TOKEN);
+
+const api_host = "https://api.vk.com/method/";
 
 const app = require("express")();
-app.listen(process.env.PORT);
+const parser = require("body-parser");
+const request = require("request");
+
+app.use(parser());
+
+app.use((req, res, next) => {
+    u.log(body);
+    next();
+})
+
+app.post("/", handler);
+app.get("/", (req, res, next) => {
+    res.status(200).contentType("text/html").send("<!DOCTYPE html>" +
+        "<meta charset=utf8><title>The secret project..</title><h1>Goodbye, World!!!</h1>" +
+        "<h2>Coming soon...</h2><h4>Congratulations! Server is running!</h4>");
+});
+
+function handler(req, res) {
+    let body = req.body;
+    if (body.type in u.types) {
+        u.types[body.type](req, res);
+    } else {
+        u.send_ok(res);
+    }
+};
 
 const u = {
-    
+    types: {
+        confirmation: (req, res) => {
+            res.status(200).send(process.env.CONFIRMATION_TOKEN);
+        },
+        message_new: (req, res) => {
+            let user_id = req.body.object.user_id;
+            let message = req.body.object.body;
+            u.send_ok(res);
+
+            u.use_method("message.send", {
+                user_id: user_id,
+                message: message
+            });
+        }
+    },
     date: () => {
         let date = new Date();
         return "[ " + date.toUTCString() + " ]";
     },
-    
     log: (...args) => {
         console.log(date(), ...args);
+    },
+    send_ok: (res) => {
+        res.status(200).send("ok");
+    },
+    stringify_params: (params) => {
+        params.v = "5.0";
+        params.access_token = process.env.TOKEN;
+        let result = [];
+        for (let i in params) {
+            result.push("" + i + params[i]);
+        }
+        return result.join("&");
+    },
+    use_method: (method, params) => {
+        request(api_host + method + "?" + u.stringify_params(params), {
+            method: "GET"
+        });
     }
 }
-
-bot.on((data) => {
-    u.log(data);
-    data.reply();
-});
-
-bot.listen();
