dfa2c177fcef63e5a5acd024d750233ec1104f1c
pettro98 <ya.skywalker@yandex.ru>
2018-01-11 23:27:10 +0300

    updat

--- bot.js
+++ bot.js
@@ -8,10 +8,14 @@ app.use((req, res, next) => {
     next();
 });
 
-app.all("/", (req, res, next) => {
+app.get("/", (req, res, next) => {
     res.status(200).contentType("text/html").send("<!DOCTYPE html>" +
         "<meta charset=utf8><title>Demo page</title><h1>Hello World!!!</h1>" +
-        "<h6>this is a demo page</h6>");
+        "<h4>this is a demo page</h4>");
+});
+
+app.post("/", (req, res, next) => {
+    res.status(200).send("a5efe228");
 });
 
 app.listen(process.env.PORT || 80, () => {
