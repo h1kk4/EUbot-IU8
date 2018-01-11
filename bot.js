78e2c71ebe3790b3c8836a0274c22da2d4cfabdd
pettro98 <ya.skywalker@yandex.ru>
2018-01-11 22:41:25 +0300

    update server

--- bot.js
+++ bot.js
@@ -1,30 +1,17 @@
 "use strict";
 
 const app = require("express")();
-const converter = require("html2canvas");
-const dom = require("jsdom");
-
-function saveToImage(name, canvas) {
-    console.log("s2img");
-    let img = canvas.toDataURL();
-    var data = img.replace(/^data:image\/\w+;base64,/, "");
-    var buf = new Buffer(data, 'base64');
-    fs.writeFile(__dirname + images + name + ".png", buf);
-}
 
 app.use((req, res, next) => {
-    console.log(req.body, "body");
+    console.log("[ " + new Date().toUTCString() + " ]",
+        "Received request:\n\t", req.method, req.url);
     next();
 });
 
 app.post("/", (req, res, next) => {
-
-    let element = dom.JSDOM.fragment("<h1>Hello world!</h1>");
-    converter(element).then((canvas) => {
-        saveToImage("img", canvas);
-    });
-    console.log("created image");
-    res.sendStatus(200);
+    res.status(200).send("Hello World!!!");
 });
 
-app.listen(5000);
+app.listen(process.env.PORT, () => {
+    console.log("Server listening on port " + process.env.PORT);
+});
